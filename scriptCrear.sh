45bbe0e547a344225d3d10d85ddd30d72d7019f6
Tsubasa1218 <juan.suaza1218@gmail.com>
2016-07-17 11:38:29 -0500

    Update scriptCrear.sh

--- scriptCrear.sh
+++ scriptCrear.sh
@@ -1,5 +1,10 @@
 
 #!/usr/bin/bash
+RAM=`echo $2 | awk '{print $1}'`
+CPU=`echo $3 | awk '{print $1}'`
+
 VBoxManage createvm --name $1 --register
-VBoxManage modifyvm $1 --vram $2 --cpus $3
+VBoxManage modifyvm $1 --vram $RAM --cpus $CPU
 VBoxManage modifyvm $1 --nic1 bridged --bridgeadapter1 e1000g0
+
+echo "Creada!!!"
